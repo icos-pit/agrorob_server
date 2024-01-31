e48b47eebc4ad7536cb06e133b95e9184876a1fe
iman01 <iman.esfandiyar71@gmail.com>
2024-01-31 09:21:53 +0100

    master branch zenoh conf from os var

--- core/views.py
+++ core/views.py
@@ -97,8 +97,8 @@ class MAG_Log:
 
 # Initiate the zenoh-net API
 # session = zenoh.open({"mode":"client","connect":{"endpoints": ["tcp/57.129.16.234:7447"]}})
-# session = zenoh.open({"mode":"client","connect":{"endpoints": [os.environ["ZENOH_URL"]]}})
-session = zenoh.open({})
+session = zenoh.open({"mode":"client","connect":{"endpoints": [os.environ["ZENOH_URL"]]}})
+# session = zenoh.open({})
 
 
 
